3e2b8f3f511e8a497c8001b22c36d630c50d36bf
Radoslaw Zajaczkowski <radoslaw.x.zajaczkowski@gsk.com>
2025-11-27 11:12:42 +0000

    Reorganised files per  OS

---
+++ shell-configs/colors.sh
@@ -0,0 +1,13 @@
+# Shared color definitions for shell prompts
+
+# Bash color codes
+export BASH_CYAN='\[\e[36m\]'
+export BASH_YELLOW='\[\e[33m\]'
+export BASH_GREEN='\[\e[32m\]'
+export BASH_RESET='\[\e[0m\]'
+
+# Zsh color codes
+export ZSH_CYAN='%F{cyan}'
+export ZSH_YELLOW='%F{yellow}'
+export ZSH_GREEN='%F{green}'
+export ZSH_RESET='%f'
